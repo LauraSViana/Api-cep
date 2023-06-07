b0f02bad0d6c6691bcd3aef1b88a9e5c460d27db
LauraViana <123893063+LauraSViana@users.noreply.github.com>
2023-06-07 12:12:00 -0400

    Add files via upload

---
+++ cep/index.js
@@ -0,0 +1,14 @@
+const btnCep = document.getElementById('btnCep');
+
+btnCep.addEventListener('click', () => {
+    const cep = document.getElementById('cep').value;
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then((response) => {
+        return response.json();
+    }).then((data) => {
+        document.getElementById('rua').value = data.logradouro;
+        document.getElementById('bairro').value = data.bairro;
+        document.getElementById('municipio').value = data.localidade;
+        document.getElementById('estado').value = data.uf;
+       
+    });
+});
